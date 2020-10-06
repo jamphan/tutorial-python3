1e084ba2d18ef75b5f8baaaaa625a50963543b83
Jamie Phan <jamie.kt.phan@gmail.com>
2020-10-07 10:10:41 +1100

    Add ex answers

---
+++ exercises/answers/04b.py
@@ -0,0 +1,10 @@
+age = int(input("How old are you?"))
+
+if (age > 25):
+    print("You're probably having a mid-life crisis right now")
+elif (age > 19):
+    print("You're a young adult")
+if (age > 10):
+    print("You're a teenager")
+else:
+    print("You're getting old")
