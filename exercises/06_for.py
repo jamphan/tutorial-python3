d00e39409a02af7815a32d73efbb9d9f544aae2a
Jamie Phan <jamie.kt.phan@gmail.com>
2020-10-07 10:12:59 +1100

    add for loop

---
+++ exercises/06_for.py
@@ -0,0 +1,15 @@
+all_star_war_movies = [
+    "A New Hope",
+    "The Empire Strikes Back",
+    "Return of the Jedi",
+    "The Phantom Menace",
+    "Attack of the Clones",
+    "Revenge of the Sith",
+    "The Force Awakens",
+    "The Last Jedi",
+    "The Rise of the Skywalker"
+]
+
+print("The Star Wars saga include the movies:"
+for mv in all_star_war_movies:
+    print("\t" + mv)
