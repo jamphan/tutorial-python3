995064272f27a13c52b9b2600ebb8d3f33103e57
Jamie Phan <jamie.kt.phan@gmail.com>
2020-10-14 11:04:06 +1100

    Update explaination for escape sequences

--- exercises/06_for.py
+++ exercises/06_for.py
@@ -7,9 +7,12 @@ Summary:
     - We are using Python's 'in' keyword to loop through a list
 
 Explaination:
-    - line 17: We define a simple list of star wars movies
-    - Line 30: Read aloud: "For every element in the all_star_war_movies list, assign it to the
+    - line 20: We define a simple list of star wars movies
+    - Line 33: Read aloud: "For every element in the all_star_war_movies list, assign it to the
         loop variable 'mv' and perform the functions in the subsequent block".
+    - Line 34: the "\t" is called an "escape sequence". When python sees the slash it knows to treat
+        the following charater different. In this case, it knows that the programmar does not want
+        to use the literal "t" character, instead they want the escaped tab character.
 
 """
 # ==================================================================================================
