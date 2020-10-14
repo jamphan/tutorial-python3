4b57d6cea1a8d103aa5211ff9281cf468c4ebe6a
Jamie Phan <jamie.kt.phan@gmail.com>
2020-10-14 11:05:36 +1100

    Add additional escape sequence examples

--- exercises/06_for.py
+++ exercises/06_for.py
@@ -7,12 +7,13 @@ Summary:
     - We are using Python's 'in' keyword to loop through a list
 
 Explaination:
-    - line 20: We define a simple list of star wars movies
-    - Line 33: Read aloud: "For every element in the all_star_war_movies list, assign it to the
+    - line 21: We define a simple list of star wars movies
+    - Line 34: Read aloud: "For every element in the all_star_war_movies list, assign it to the
         loop variable 'mv' and perform the functions in the subsequent block".
-    - Line 34: the "\t" is called an "escape sequence". When python sees the slash it knows to treat
+    - Line 35: the "\t" is called an "escape sequence". When python sees the slash it knows to treat
         the following charater different. In this case, it knows that the programmar does not want
         to use the literal "t" character, instead they want the escaped tab character.
+        Other escape sequences include "\b" (backspace), "\n" (newline), "\\" (backslash)
 
 """
 # ==================================================================================================
